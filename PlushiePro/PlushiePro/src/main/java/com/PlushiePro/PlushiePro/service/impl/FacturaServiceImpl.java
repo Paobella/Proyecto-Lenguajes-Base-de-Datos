d87acf611af6adb06fed81e17bf3e447825b81e9
Emmanuel8112 <145504467+Emmanuel8112@users.noreply.github.com>
2024-08-07 13:15:36 -0600

    Factura corregido

--- PlushiePro/PlushiePro/src/main/java/com/PlushiePro/PlushiePro/service/impl/FacturaServiceImpl.java
+++ PlushiePro/PlushiePro/src/main/java/com/PlushiePro/PlushiePro/service/impl/FacturaServiceImpl.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.PlushiePro.PlushiePro.service.impl;
 
 import com.PlushiePro.PlushiePro.dao.FacturaDao;
 import com.PlushiePro.PlushiePro.domain.Factura;
 import com.PlushiePro.PlushiePro.service.FacturaService;
-import jakarta.persistence.*;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,14 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class FacturaServiceImpl implements FacturaService {
 
-     @PersistenceContext
-    private EntityManager entityManager;
+    @Autowired
+    private FacturaDao facturaDao;
 
     @Override
     @Transactional(readOnly = true)
     public List<Factura> getFacturas() {
-        StoredProcedureQuery storedProcedure = entityManager.createStoredProcedureQuery("GET_FACTURAS", Factura.class);
-        storedProcedure.execute();
-        return storedProcedure.getResultList();
+        var lista = facturaDao.findAll();
+        return lista;
     }
 }
